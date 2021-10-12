c4e3a848cce846dcbc45f6e91b10dfc63fe2a1b3
Muhammad Rashud <muhammad.rashid@cooper.edu>
2021-10-12 16:29:38 +0000

    adding HW2

--- HW2/prob-1/myCode.c
+++ HW2/prob-1/myCode.c
@@ -1,5 +1,19 @@
 #include <stdio.h>
+#define SUCCESS 0
 
 int main(void) {
-	printf("Hello, World!\n");
+	
+	double a, b;
+	float perimeter;
+
+	printf("Enter Length = ");
+	scanf("%lf", &a);
+	printf("Enter Width = ");
+	scanf("%lf", &b);
+	
+	perimeter = (2*a + 2*b);
+
+	printf("Perimeter of Rectangle = %0.2f\n", perimeter);
+
+	return(SUCCESS);
 }
